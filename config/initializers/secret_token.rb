938bf5cc67488c59d937b618c277990fa2408bf2
Matthew Rothenberg <mrothenberg@gmail.com>
2011-12-31 00:58:40 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodvsevil::Application.config.secret_token = 'a02082822203f1c6afb5dbbea3291ecd891424956e443524ea4cfce14e027602a351b9112001c9796a2ed2da0b300c47f4d0688f3aa54b1de8f03954eb8ef8e4'
