d13595490d94376449ab5679785dd29e853a1077
Matthew Rothenberg <mrothenberg@gmail.com>
2011-12-31 01:30:26 -0500

    working controller that responds

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,18 @@
+class SiteController < ApplicationController
+  def index
+    @count = REDIS.mget 'cat_count', 'dog_count'
+    @cat_count = @count[0]
+    @dog_count = @count[1]
+    
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { 
+        render :json  => { 
+          :cat_count  => @cat_count, 
+          :dog_count  => @dog_count 
+        } 
+      }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Goodvsevil::Application.routes.draw do
+  get "site/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
