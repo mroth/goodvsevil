acabb2f5f66b359e39e6145ba91928e364872599
Matthew Rothenberg <mrothenberg@gmail.com>
2012-09-08 11:46:01 -0400

    add newrelic to production for web

--- web.rb
+++ web.rb
@@ -8,6 +8,9 @@ configure do
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
+configure :production do
+  require 'newrelic_rpm'
+end
 conns = []
 
 get '/' do
